40a89fe681bccdd691dc6c4103b9bf2527321340
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-03-15 12:05:34 +0000

    first html file and favicon added in app folders -template and static in development, to be changed for production deployment

--- fastfood/views.py
+++ fastfood/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def first_testview(request):
+    return render(request, 'fastfood/fastfood_home.html')
--- portfolio4_fastfood/urls.py
+++ portfolio4_fastfood/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from fastfood.views import first_testview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', first_testview, name='first_testview')
 ]
