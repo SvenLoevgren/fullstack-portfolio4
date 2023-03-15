7b7b7f7ab07123f902a6080fed05c1776f77ec4f
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-03-15 14:33:09 +0000

    static folder moved in repository to checkHeruko deployment favicon image

--- portfolio4_fastfood/urls.py
+++ portfolio4_fastfood/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from fastfood.views import first_testview
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', first_testview, name='first_testview')
