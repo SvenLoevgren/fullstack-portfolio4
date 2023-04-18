64ad355192ebc3a97d401875045f50086329e941
SvenLoevgren <sven.lovgren.76@gmail.com>
2023-04-18 13:44:24 +0000

    email field changed in models.py before continuing adding availability madule

--- fastfood/models.py
+++ fastfood/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Booking(models.Model):
     customer_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(max_length=255)
     phone_number = models.CharField(max_length=20)
     date = models.DateField()
     time = models.TimeField()
